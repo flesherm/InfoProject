a91b45009dcf4e721f10cea1455a789d6e74ae9c
Matthew Flesher <Matthew Flesher@F100134843.ad.faa.gov>
2018-11-29 15:51:55 -0500

    minor change

--- src/main/java/com/infoproject/CreateHuffmanDisplayAction.java
+++ src/main/java/com/infoproject/CreateHuffmanDisplayAction.java
@@ -31,31 +31,36 @@ public class CreateHuffmanDisplayAction implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         String ens = ensembleTextArea.getText();
         String prob = probabilitiesTextArea.getText();
-        //Parse into arrays
-        String[] ensemble = ArrayParserUtils.parseCommaSeparatedStrings(ens);
-        String[] probs = ArrayParserUtils.parseCommaSeparatedStrings(prob);
-        double[] p = ArrayParserUtils.parseIntoArrayOfDoubles(probs);
-        //convert probs to array of doubles
-        //Check they are same size and the probs add up to 1.0
-        if(ArrayParserUtils.arraysAreSameLength(ensemble, probs) &&
-                ArrayParserUtils.sumIsOne(p)){
-            HuffmanEncoder huffman = new HuffmanEncoder(p, ensemble);
-            Map<String, String> encodings = huffman.encodeHuffman();
-            MapUtils.printMap(encodings);
-            //Calculate Entropy
-            double[] ent = EntropyCalculator.calculateEntropyOfEach(p);
-            ArrayParserUtils.printDoubleArray(ent);
-            double entEns = EntropyCalculator.calculateEntropyOfEnsemble(p);
-            System.out.println("Entropy of ensemble: " + EntropyCalculator
-                    .roundTo4decimalPoints(entEns));
-            //TODO: Display values and entropy and L
-        }else {
-            //TODO: Display message saying they are not the same
-            System.out.println("The length of the arrays must be equal and the sum "
-                    + "of the probabilities must add up to 1.0.");
+        if(ens != null && !ens.isEmpty() && prob != null && !prob.isEmpty()){
+            //Parse into arrays
+            String[] ensemble = ArrayParserUtils.parseCommaSeparatedStrings(ens);
+            String[] probs = ArrayParserUtils.parseCommaSeparatedStrings(prob);
+            double[] p = ArrayParserUtils.parseIntoArrayOfDoubles(probs);
+            //convert probs to array of doubles
+            //Check they are same size and the probs add up to 1.0
+            if(ArrayParserUtils.arraysAreSameLength(ensemble, probs) &&
+                    ArrayParserUtils.sumIsOne(p)){
+                HuffmanEncoder huffman = new HuffmanEncoder(p, ensemble);
+                Map<String, String> encodings = huffman.encodeHuffman();
+                MapUtils.printMap(encodings);
+                //Calculate Entropy
+                double[] ent = EntropyCalculator.calculateEntropyOfEach(p);
+                ArrayParserUtils.printDoubleArray(ent);
+                double entEns = EntropyCalculator.calculateEntropyOfEnsemble(p);
+                System.out.println("Entropy of ensemble: " + EntropyCalculator
+                        .roundTo4decimalPoints(entEns));
+                //TODO: Display values and entropy and L
+            }else {
+                //TODO: Display message saying they are not the same
+                System.out.println("The length of the arrays must be equal and the sum "
+                        + "of the probabilities must add up to 1.0.");
+            }
+            String message = String.format("You entered: {'%s'}, {'%s'}", ens, prob);
+            System.out.println(message);
+        }else{
+            String message = "You must enter data in all fields.";
+            System.out.println(message);
         }
-        String message = String.format("{'%s'}, {'%s'}", ens, prob);
-        System.out.println(message);
     }
     
 }
